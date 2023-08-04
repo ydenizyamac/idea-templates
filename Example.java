b96f0c78e5b187f23a4da56dc19729b28d9569ab
Deniz Yamaç <ydenizyamac@gmail.com>
2023-08-05 02:40:31 +0300

    add group

--- Example.java
+++ Example.java
@@ -1,5 +1,5 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
 #parse("File Header.java")
 public class Example${NAME} {
-    private final String model = "${MODEL_NAME}";
+    private final String model = "${Model_Name}";
 }
