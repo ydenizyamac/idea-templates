0b47903da60925a6e0fa63b465807805dda85082
Deniz Yamaç <ydenizyamac@gmail.com>
2023-08-04 20:03:52 +0300

    template java issue fix

--- Example.java
+++ Example.java
@@ -1,5 +1,5 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
 #parse("File Header.java")
 public class Example${NAME} {
-    private readonly String model = "${modelName}";
+    private final String model = "${modelName}";
 }
