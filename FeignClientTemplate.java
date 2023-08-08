f3d36e637cd0e3c60ba58af1b80068b6129a5ce7
Deniz <ydenizyamac@gmail.com>
2023-08-09 02:07:05 +0300

    up

--- FeignClientTemplate.java
+++ FeignClientTemplate.java
@@ -1,6 +1,11 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 #parse("File Header.java")
-@FeignClient(name = "${FEIGN_NAME}", url = "${FEIGN_URL}")
+@FeignClient(name = "${Feign_Name}", url = "${Feign_Url}")
 public interface ${NAME} {
     
 }
