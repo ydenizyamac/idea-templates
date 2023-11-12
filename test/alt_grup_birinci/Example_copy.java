26e1b0db389fd28e906dea99d99307c62c92a84e
Deniz Yamaç <ydenizyamac@gmail.com>
2023-11-12 19:35:54 +0300

    enum

--- test/alt_grup_birinci/Example_copy.java
+++ test/alt_grup_birinci/Example_copy.java
@@ -1,10 +1,19 @@
-#set ($EnumValuesExample = ["VALUE1", "VALUE2", "VALUE3"])
+
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
 #parse("File Header.java")
 public class Example${NAME} {
+    #if (${ENUM})
+    enum MyEnum {
+        VALUE1,
+        VALUE2,
+        VALUE3
+    }
+    #end
     private final String model = "${Model_Name}";
     private final String model2 = "${Model}";
     private final String model3 = "${Model.Name}";
-    private final String model4 = $EnumValuesExample;
-    private final String model5 = "$EnumValuesExample";
+    @Variable(enumType = MyEnum.class)
+    private String myVariable;
 }
+
+
